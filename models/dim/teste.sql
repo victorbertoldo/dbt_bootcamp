e4ae7b24aaf9e84f5677dd7efa27b03d61c79f9d
victorbertoldo <eu_viktor@hotmail.com>
2022-08-22 23:12:59 -0300

    teste

---
+++ models/dim/teste.sql
@@ -0,0 +1,12 @@
+{% set query %}
+    select 
+    *
+    from AIRBNB.DEV.FCT_REVIEWS
+    limit 10
+{% endset %}
+
+{% set results=run_query(query) %}
+{%- if execute-%}
+    {{ results.pivot(key='REVIEW_DATE', pivot='REVIEW_SENTIMENT') }}
+
+{%- endif -%}    

57a056e67331f89722ff4e1d7bfd112ba039c12a
Victor Bertoldo <eu_viktor@hotmail.com>
2022-09-26 11:51:52 -0300

    t

---
+++ models/pivot/teste_.sql
@@ -0,0 +1,22 @@
+
+select * from (
+    select 
+        o.O_ORDERSTATUS, o.O_TOTALPRICE, o.O_ORDERDATE 
+    from {{ source('sample', 'ORDERS') }} o    
+    {# union
+    select
+        'A' as O_ORDERSTATUS,
+        99.99 as O_TOTALPRICE,
+        '1992-01-01' as O_ORDERDATE #}
+        
+    ) t order by O_ORDERSTATUS, O_ORDERDATE 
+PIVOT (
+    sum(O_TOTALPRICE)
+    for O_ORDERSTATUS in (
+        'P', 'F', 'O'
+    )
+)
+as p (Data, Partiu, Ficou, Outros)
+
+{{ this.database }}
+{{ this.schema }}
